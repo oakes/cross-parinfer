c4c1d45f7605834fb63f87fc6346737b0dcf8380
oakes <zsoakes@gmail.com>
2016-05-01 20:16:03 -0400

    Start 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cross-parinfer "1.0.0"
+(defproject cross-parinfer "1.0.1-SNAPSHOT"
   :description "A library that wraps Parinfer for Clojure and ClojureScript"
   :url "https://github.com/oakes/cross-parinfer"
   :license {:name "Public Domain"

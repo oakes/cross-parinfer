77f94095b9d08e6773313c232a0a3515526ae9c3
oakes <zsoakes@gmail.com>
2016-06-21 18:06:28 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cross-parinfer "1.1.7-SNAPSHOT"
+(defproject cross-parinfer "1.1.8"
   :description "A library that wraps Parinfer for Clojure and ClojureScript"
   :url "https://github.com/oakes/cross-parinfer"
   :license {:name "Public Domain"
